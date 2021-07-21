ec23196310d873907609b4a4805c7829c6c6eafe
Tom Guerney <tguerney@gmail.com>
2021-07-21 21:53:52 +1000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM kalilinux/kali-rolling
+
+RUN apt-get update -y && \
+    apt-get install openvpn -y
+
+ARG CONFIG_FILE
+
+COPY ${CONFIG_FILE} /home/config.ovpn
+
+# ENTRYPOINT [ "cat", "/home/config.ovpn" ]
+
+ENTRYPOINT ["openvpn", "/home/config.ovpn"]
---
+++ run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+IMAGE_NAME=kustom_kali
+
+CONFIG_FILE=$1
+RUNNING_CONTAINER=$(docker ps | grep kustom_kali | awk '{print $1}')
+
+if [[ ! -f "$CONFIG_FILE" ]]; then
+  echo "$CONFIG_FILE does not exist"
+  exit 1
+fi
+
+if [[ "${CONFIG_FILE: -5}" != ".ovpn" ]]; then
+  echo "$CONFIG_FILE is not an .ovpn config file"
+  exit 1
+fi
+
+if [[ $RUNNING_CONTAINER ]]; then
+  echo "stopping $IMAGE_NAME container $RUNNING_CONTAINER..."
+  docker stop $RUNNING_CONTAINER >/dev/null 2>/dev/null
+fi
+
+EXISTING_IMAGE=$(docker images | grep $IMAGE_NAME | tr -s ' ' | cut -d ' ' -f 3)
+
+if [[ $EXISTING_IMAGE ]]; then
+  echo "removing $IMAGE_NAME image $EXISTING_IMAGE..."
+  docker rmi --force $EXISTING_IMAGE >/dev/null 2>/dev/null
+fi
+
+docker build --build-arg CONFIG_FILE=$CONFIG_FILE -t $IMAGE_NAME . >/dev/null 2>/dev/null
+
+echo "using openvpn config $CONFIG_FILE..."
+
+echo "running new $IMAGE_NAME container..."
+
+NEW_CONTAINER=$(docker run -d --rm \
+  --cap-add=NET_ADMIN \
+  --device /dev/net/tun \
+  --sysctl net.ipv6.conf.all.disable_ipv6=0 \
+  $IMAGE_NAME)
+
+echo "$NEW_CONTAINER running..."
+
+echo "starting interactive session..."
+
+docker exec -it $NEW_CONTAINER bash
+
+echo "stopping container $NEW_CONTAINER..."
+
+docker stop $NEW_CONTAINER >/dev/null 2>/dev/null
+
+RUNNING_CONTAINER=$(docker ps | grep kustom_kali | awk '{print $1}')
+
+if [[ $RUNNING_CONTAINER ]]; then
+  echo "Unable to stop $IMAGE_NAME container $RUNNING_CONTAINER..."
+fi
